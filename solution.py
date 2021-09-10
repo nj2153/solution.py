da219a662a021636a3206cac7f8ff06bdd34c4ca
nj2153 <90479666+nj2153@users.noreply.github.com>
2021-09-10 19:28:52 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def welcome_assignment_answers(question):
     #Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the channel posted by a TA?":
         answer = "mTLS"
-    if question == "Are encoding and encryption the same? - Yes/No":
+    elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
