f6ab8eb6bc1cbec1866782aeab6b701aba015d40
nj2153 <90479666+nj2153@users.noreply.github.com>
2021-09-11 09:43:24 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
+    if question == "In Slack, what is the secret passphrase posted in the # cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
